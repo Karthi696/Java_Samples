9556f846584a5580632087d3d91d0ba77d7ff761
Karthi696 <karthi696@gmail.com>
2016-02-04 14:19:25 +0530

    Create src.java

---
+++ src.java
@@ -0,0 +1,14 @@
+public class src {                         
+    public String sourceStringForInteger;
+    public String getSourceStringForInteger() {
+		return sourceStringForInteger;
+	}
+	public void setSourceStringForInteger(String sourceStringForInteger) {
+		this.sourceStringForInteger = sourceStringForInteger;
+	}
+	src(String a){
+    	this.sourceStringForInteger = a;
+    }
+    
+    
+}
