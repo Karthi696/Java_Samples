ba643e490d8bb70d1e63af54962d51a5acfd5ae2
Karthi696 <karthi696@gmail.com>
2016-02-04 14:17:14 +0530

    Create ArrayList_sample.java

---
+++ ArrayList_sample.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+
+public class PlayArray2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> al = Arrays.asList("hi", "bi");
+		System.out.println(al);
+	}
+
+}
