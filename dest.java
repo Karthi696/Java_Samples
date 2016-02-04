6d44c0ae85c57cabec827685b3f5b615edce232a
Karthi696 <karthi696@gmail.com>
2016-02-04 14:19:55 +0530

    Create dest.java

---
+++ dest.java
@@ -0,0 +1,14 @@
+
+public class dest {
+	public String sourceStringForSecond;
+
+	public String getSourceStringForSecond() {
+		return sourceStringForSecond;
+	}
+
+	public void setSourceStringForSecond(String sourceStringForSecond) {
+		this.sourceStringForSecond = sourceStringForSecond;
+	}
+
+	
+}
